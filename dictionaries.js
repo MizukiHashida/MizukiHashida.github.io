6853c2f345b7048ac2352a2cbd23a42c1a315008
Ammar Laili <muhammadammar0203@gmail.com>
2023-08-30 21:19:42 +0800

    basic search function added. list items of results now redirect to respective pages

---
+++ dictionaries.js
@@ -0,0 +1,15 @@
+var idToParkName = {
+    0: "Woodlands Waterfront Park"
+};
+
+var idToParkImageName = {
+    0: "Images/background1.jpg"
+};
+
+var idToParkDescription = {
+    0: "This park is the nearest park to Johor Bahru."
+};
+
+var idToParkLink = {
+    0: "Pages/Woodlands_Waterfront_Park.html"
+};
