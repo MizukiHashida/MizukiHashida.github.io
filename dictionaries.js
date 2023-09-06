f2555e5a773b1512f2d4f30152ca314ff1ad13d4
Ammar Laili <muhammadammar0203@gmail.com>
2023-09-06 18:32:01 +0800

    Botanic Gardens Page added and some content added. dictionaries updated

--- dictionaries.js
+++ dictionaries.js
@@ -1,15 +1,19 @@
 var idToParkName = {
-    0: "Woodlands Waterfront Park"
+    0: "Woodlands Waterfront Park",
+    1: "Singapore Botanic Gardens"
 };
 
 var idToParkImageName = {
-    0: "Images/background1.jpg"
+    0: "Images/background1.jpg",
+    1: "Images/Botanic_Gardens/Preview.jpg"
 };
 
 var idToParkDescription = {
-    0: "This park is the nearest park to Johor Bahru."
+    0: "This park is the nearest park to Johor Bahru.",
+    1: "Located at the heart of Singapore, The Singapore Botanic Gardens is one of three gardens in the world honoured as a UNESCO World Heritage Site. "
 };
 
 var idToParkLink = {
-    0: "Pages/Woodlands_Waterfront_Park.html"
+    0: "Pages/Woodlands_Waterfront_Park.html",
+    1: "Pages/Botanic_Gardens.html"
 };
